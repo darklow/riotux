2801244f74f5e70068e75c2a7dd7f1fe34f18873
luisvinicius167 <dev_luis@hotmail.com>
2016-03-24 22:40:34 -0300

    page ok

---
+++ js/stores.js
@@ -0,0 +1,26 @@
+/** Add the Stores */
+var carStore = new CarStore();
+riotux.addStore('carStore', carStore);
+
+var personStore = new PersonStore();
+riotux.addStore('personStore', personStore);
+
+
+/** Stores */
+function CarStore ( ) {
+  riot.observable(this);
+  // listen to 'start' event
+  this.on('start', function ( person ) {
+    console.log(person + ' started the car.')
+    riotux.emmit('carMoving', person);
+  });
+};
+
+function PersonStore ( ) {
+  riot.observable(this);
+
+  // listen to 'startCar' event
+  this.on('startCar', function ( person ) {
+    riotux.trigger('carStore', 'start', person);
+  });
+};
